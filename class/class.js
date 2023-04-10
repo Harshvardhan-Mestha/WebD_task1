83f3084aec4dc38fec5be4ca9144df6081224754
Harshvardhan Mestha <harshvardhanmestha@yahoo.com>
2023-04-10 19:02:58 +0530

    This commit adds the code for the weekly timetable.

--- class/class.js
+++ class/class.js
@@ -1,2 +1,17 @@
 
 
+
+
+let cards = document.querySelectorAll(".card")
+console.log(cards)
+cards.forEach((card) => {
+    if(card.innerHTML == "CP"){card.style.backgroundColor = "orange"}
+    if(card.innerHTML == "M2"){card.style.backgroundColor = "crimson"}
+    if(card.innerHTML == "ES"){card.style.backgroundColor = "blue"}
+    if(card.innerHTML == "MOW"){card.style.backgroundColor = "dodgerblue"}
+    if(card.innerHTML == "THERMO"){card.style.backgroundColor = "lightblue"}
+    if(card.innerHTML == "BIO LAB"){card.style.backgroundColor = "lime"}
+    if(card.innerHTML == "WS"){card.style.backgroundColor = "purple"}
+
+
+})
